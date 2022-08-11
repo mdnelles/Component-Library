b53808bc83575c0e6ac649c4c7ed1ba0c80cd3e9
mdnelles <mxnelles@gmail.com>
2022-08-11 10:47:04 -0500

    seeding revision

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,56 +1,39 @@
-import React from "react";
-import { AnimWrap } from "./styles/AnimWrap";
-import { SceneStyle } from "./styles/Scene";
-import { ObjProps } from "./Faces/FaceInter";
-import Face from "./Faces/Face";
-import { ObjWrapper } from "./styles/Global";
+import React from 'react';
+import { AnimWrap } from './styles/AnimWrap';
+import { SceneStyle } from './styles/Scene';
+import { ObjProps } from './Faces/FaceInter';
+import Face from './Faces/Face';
+import { ObjWrapper } from './styles/Global';
 
 export const Card = (props: ObjProps): any => {
-   let {
-      anim1Specs = {},
-      anim2Specs = {},
-      width = 5,
-      height = 5,
-      faces = { front: true, back: true },
-      global = {},
-      custom = {},
-      tranz = (+height / 2) | 0,
-      perspective = 900,
-      perspectiveOrigin = "50% 50%",
-      zIndex = 10,
-   } = props;
+    let {
+        anim1Specs = {},
+        anim2Specs = {},
+        width = 5,
+        height = 5,
+        faces = { front: true, back: true },
+        global = {},
+        custom = {},
+        tranz = (+height / 2) | 0,
+        perspective = 900,
+        perspectiveOrigin = '50% 50%',
+        zIndex = 10
+    } = props;
 
-   const buildFace = (faceType: any): any => {
-      return (
-         <Face
-            width={width}
-            height={height}
-            depth={0.1}
-            faceType={faceType}
-            id={faceType}
-            tranz={tranz}
-            global={global}
-            custom={custom}
-         />
-      );
-   };
+    const buildFace = (faceType: any): any => {
+        return <Face width={width} height={height} depth={0.1} faceType={faceType} id={faceType} tranz={tranz} global={global} custom={custom} />;
+    };
 
-   return (
-      <SceneStyle
-         width={width}
-         height={height}
-         perspective={perspective}
-         perspectiveOrigin={perspectiveOrigin}
-         zIndex={zIndex}
-      >
-         <AnimWrap animSpecs={anim1Specs}>
-            <AnimWrap animSpecs={anim2Specs}>
-               <ObjWrapper>
-                  {!!faces && !!faces.front ? buildFace("front") : null}
-                  {!!faces && !!faces.back ? buildFace("back") : null}
-               </ObjWrapper>
+    return (
+        <SceneStyle width={width} height={height} perspective={perspective} perspectiveOrigin={perspectiveOrigin} zIndex={zIndex}>
+            <AnimWrap animSpecs={anim1Specs}>
+                <AnimWrap animSpecs={anim2Specs}>
+                    <ObjWrapper>
+                        {!!faces && !!faces.front ? buildFace('front') : null}
+                        {!!faces && !!faces.back ? buildFace('back') : null}
+                    </ObjWrapper>
+                </AnimWrap>
             </AnimWrap>
-         </AnimWrap>
-      </SceneStyle>
-   );
+        </SceneStyle>
+    );
 };
--- src/components/Ribbon.tsx
+++ src/components/Ribbon.tsx
@@ -1,65 +1,48 @@
-import React from "react";
-import { AnimWrap } from "./styles/AnimWrap";
-import { SceneStyle } from "./styles/Scene";
-import { ObjProps } from "./Faces/FaceInter";
-import Face from "./Faces/Face";
-import { ObjWrapper } from "./styles/Global";
+import React from 'react';
+import { AnimWrap } from './styles/AnimWrap';
+import { SceneStyle } from './styles/Scene';
+import { ObjProps } from './Faces/FaceInter';
+import Face from './Faces/Face';
+import { ObjWrapper } from './styles/Global';
 
 export const Ribbon = (props: ObjProps): any => {
-   let {
-      anim1Specs = {},
-      anim2Specs = {},
-      width = 5,
-      height = 5,
-      depth = 5,
-      faces = {
-         front: true,
-         back: true,
-         right: true,
-         left: true,
-         top: true,
-         bottom: true,
-      },
-      global = {},
-      custom = {},
-      tranz = (+height / 2) | 0,
-      perspective = 900,
-      perspectiveOrigin = "50% 50%",
-      zIndex = 10,
-   } = props;
+    let {
+        anim1Specs = {},
+        anim2Specs = {},
+        width = 5,
+        height = 5,
+        depth = 5,
+        faces = {
+            front: true,
+            back: true,
+            right: true,
+            left: true,
+            top: true,
+            bottom: true
+        },
+        global = {},
+        custom = {},
+        tranz = (+height / 2) | 0,
+        perspective = 900,
+        perspectiveOrigin = '50% 50%',
+        zIndex = 10
+    } = props;
 
-   const buildFace = (faceType: any): any => {
-      return (
-         <Face
-            width={width}
-            height={height}
-            depth={depth}
-            faceType={faceType}
-            id={faceType}
-            tranz={tranz}
-            global={global}
-            custom={custom}
-         />
-      );
-   };
+    const buildFace = (faceType: any): any => {
+        return <Face width={width} height={height} depth={depth} faceType={faceType} id={faceType} tranz={tranz} global={global} custom={custom} />;
+    };
 
-   return (
-      <SceneStyle
-         width={width}
-         height={height}
-         perspective={perspective}
-         perspectiveOrigin={perspectiveOrigin}
-         zIndex={zIndex}
-      >
-         <AnimWrap animSpecs={anim1Specs}>
-            <AnimWrap animSpecs={anim2Specs}>
-               <ObjWrapper>
-                  {!!faces && !!faces.bottom ? buildFace("bottom") : null}
-                  {!!faces && !!faces.back ? buildFace("back") : null}
-                  {!!faces && !!faces.top ? buildFace("topr") : null}
-               </ObjWrapper>
+    return (
+        <SceneStyle width={width} height={height} perspective={perspective} perspectiveOrigin={perspectiveOrigin} zIndex={zIndex}>
+            <AnimWrap animSpecs={anim1Specs}>
+                <AnimWrap animSpecs={anim2Specs}>
+                    <ObjWrapper>
+                        {!!faces && !!faces.bottom ? buildFace('bottom') : null}
+                        {!!faces && !!faces.back ? buildFace('back') : null}
+                        {!!faces && !!faces.top ? buildFace('topr') : null}
+                    </ObjWrapper>
+                </AnimWrap>
             </AnimWrap>
-         </AnimWrap>
-      </SceneStyle>
-   );
+        </SceneStyle>
+    );
 };
